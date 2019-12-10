f2f688c8d1349a25f4a3c5d227ce0dd14953fa86
Michael Burman <yak@iki.fi>
2019-12-10 10:12:04 +0200

    Use JBossMarshalling for Infinispan 10

--- engine/src/main/java/org/hawkular/alerts/engine/impl/ispn/model/ModelContextInitializer.java
+++
@@ -1,8 +0,0 @@
-package org.hawkular.alerts.engine.impl.ispn.model;
-
-import org.infinispan.protostream.SerializationContextInitializer;
-import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
-
-@AutoProtoSchemaBuilder(includeClasses = { IspnActionPlugin.class }, schemaPackageName = "org.hawkular.alerts.engine.impl.ispn.model")
-interface ModelContextInitializer extends SerializationContextInitializer {
-}
